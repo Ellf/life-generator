2f9723ecad5e3d6eaea3105a16131ef8ab91ec9a
ellf <tom@purplehippo.io>
2022-05-01 17:59:43 +0100

    updated readme

--- src/index.js
+++ src/index.js
@@ -151,21 +151,6 @@ createEnum([
 
 
 function createGenome(glength) {
-    // genomes are collection of genes 
-    // one gene = 8-hexadecimal digits ########
-    // 32 binary bits of data
-    // f 1 3 5 1 f e 3
-    /*
-    [1|1|1|1] [0|0|0|1] [0|0|1|1] [0|1|0|1] 
-    [0|0|0|1] [1|1|1|1] [1|1|1|0] [0|0|1|1]
-
-    bit 1       = source type [input sensory or internal neuron]
-    bit 2 - 8   = source ID (take modulo of number of neurons to find out which one it refers to????)
-    bit 9       = sink (action) type
-    bit 10 - 16 = sink ID
-
-    bit 17 - 32 = 16-bit signed integer weight of the connection (divide this by 8000 or so) to get to a floating point value -4.0 -> 4.0
-    */
 
     let gnome = [];
     for (let g = 0; g < glength; g+=1) {
